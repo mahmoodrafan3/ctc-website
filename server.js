21f94ff4c0c0e136af3f51a9e1678eb7bf93e031
mahmoodrafan3 <mahmoodrafan3@gmail.com>
2026-07-01 16:22:15 +0530

    Initial commit: CTC Strategy website with TradingView & StrategyTune indicator code

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for /code - serve code.html
+app.get('/code', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'code.html'));
+});
+
+// All other routes go to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`CTC Strategy Website running at http://localhost:${PORT}`);
+});
